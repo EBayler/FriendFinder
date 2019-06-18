d4810636bd87d6ed530a9870dbf3b8a3e82ef625
EBayler <erika.bayler@gmail.com>
2019-06-17 18:22:47 -0600

    updated answers ans css styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
     module.exports = [{
             name: "Swamp Thing",
             photo: "https://media.giphy.com/media/b5ZNou8CKvBCg/giphy.gif",
-            answers: [3, 1, 1, 1, 5, 4, 1, 4, 4, 5]
+            answers: [1, 3, 5, 1, 4, 2, 3, 1, 1, 1, 4, 5]
         },
         {
             name: "Willow",
             photo: "https://thumbs.gfycat.com/ImpassionedDecisiveBeardeddragon-size_restricted.gif",
-            answers: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+            answers: [4, 4, 3, 3, 2, 2, 4, 4, 2, 3, 5, 5]
         },
         {
             name: "The Doctor",
             photo: "https://media1.tenor.com/images/1b5d3c6c027788c2259b75772b0e7636/tenor.gif?itemid=5404063",
-            answers: [4, 1, 2, 1, 5, 5, 1, 5, 5, 5]
+            answers: [5, 5, 3, 3, 5, 4, 2, 2, 5, 3, 5, 5]
         },
         {
             name: "Data",
             photo: "https://i.pinimg.com/originals/bc/9e/c9/bc9ec984f0fc02c61b1c0c7c76bc4322.gif",
-            answers: [5, 1, 4, 1, 5, 5, 1, 5, 5, 5]
+            answers: [1, 4, 3, 1, 1, 2, 2, 5, 1, 2, 5, 5]
         },
         {
             name: "Ms. Marvel - Kamala Khan",
             photo: "https://78.media.tumblr.com/fb38f347b21931616caa95c53f86460e/tumblr_oy2424NqSB1vqb1i2o1_540.gif",
-            answers: [5, 1, 3, 4, 5, 5, 1, 4, 3, 5]
+            answers: [3, 5, 3, 3, 1, 3, 1, 2, 1, 4, 5, 5]
         },
         {
             name: "Deadpool",
             photo: "https://media.tenor.com/images/114b50f9e5c882faba7088ea652e902a/tenor.gif",
-            answers: [4, 2, 4, 4, 5, 4, 1, 5, 5, 5]
+            answers: [5, 1, 1, 1, 1, 5, 3, 3, 5, 5, 2, 5]
         },
         {
             name: "Jar Jar Binks",
             photo: "https://media.giphy.com/media/3owzWfuEMmNX4rquJ2/giphy.gif",
-            answers: [4, 1, 1, 1, 3, 1, 1, 5, 2, 5]
+            answers: [3, 3, 3, 5, 1, 2, 1, 1, 1, 5, 3, 4]
         },
         {
             name: "Cthulhu",
             photo: "https://i.redd.it/tdum3ppd55gz.gif",
-            answers: [5, 1, 5, 1, 5, 3, 1, 4, 4, 5]
+            answers: [1, 1, 1, 1, 5, 1, 5, 1, 1, 5, 1, 5]
         },
         {
-            name: "Bob Marley",
-            photo: "https://media3.giphy.com/media/xlfEeOexKxJua6VZRh/giphy.gif",
-            answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+            name: "Skwisgaar",
+            photo: "https://i.gifer.com/KAat.gif",
+            answers: [5, 1, 1, 1, 4, 5, 4, 1, 5, 4, 1, 5]
         },
         {
             name: "Maurice Moss",
             photo: "https://media1.giphy.com/media/yqtpq8rqqXBh6/giphy.gif",
-            answers: [5, 5, 5, 3, 1, 3, 1, 5, 5, 5]
+            answers: [4, 2, 2, 3, 1, 1, 2, 5, 2, 5, 2, 5]
         },
 
     ];
